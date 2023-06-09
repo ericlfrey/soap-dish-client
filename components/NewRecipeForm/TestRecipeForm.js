678fd0d28ea1b96fe174c0f6cc9e305214712c95
Eric Frey <ericlfrey@gmail.com>
2023-06-09 12:21:33 -0500

    WIP. Got the lye amount to calculate. Need to add the Superfat amout to that.

---
+++ components/NewRecipeForm/TestRecipeForm.js
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import styles from './NewRecipeForm.module.css';
+import { getOils, getSingleOil } from '../../utils/data/oilData';
+
+export default function TestRecipeForm() {
+  const initialState = {
+    totalOil: 32,
+    oilList: [],
+    title: '',
+    waterAmount: 33,
+    lyeAmount: 0,
+    superFat: 5,
+    description: '',
+    notes: '',
+    public: false,
+  };
+  const [formInput, setFormInput] = useState(initialState);
+  const [oils, setOils] = useState([]);
+  const [allOils, setAllOils] = useState([]);
+
+  useEffect(() => {
+    getOils().then(setAllOils);
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmitTotalOil = (e) => {
+    e.preventDefault();
+  };
+
+  const addOils = () => {
+    const currentOil = document.getElementById('currentOil');
+    getSingleOil(currentOil.value).then((selectedOil) => {
+      const oilExists = oils.some((oil) => oil.id === selectedOil.id);
+      if (oilExists) {
+        window.alert('Oil already in list');
+      } else {
+        const updatedOils = [...oils, selectedOil];
+        setOils(updatedOils);
+      }
+    });
+  };
+
+  const handleAmountChange = (e) => {
+    const { name, value } = e.target;
+    const oilId = parseInt(name, 10);
+    const updatedOils = oils.map((oil) => (oil.id === oilId ? { ...oil, amount: value } : oil));
+    setOils(updatedOils);
+  };
+
+  const calculateRecipe = (e) => {
+    e.preventDefault();
+    // const recipeOils = oils.map((oil) => {
+    //   const amount = parseInt(oil.amount, 10);
+    //   return { oilId: oil.id, amount };
+    // });
+    const superFat = parseInt(formInput.superFat, 10) / 100;
+    let lyeAmount = oils.reduce((a, b) => a + b.amount * b.sap, 0);
+    lyeAmount *= (1 - superFat);
+    setFormInput((prevState) => ({
+      ...prevState,
+      // oilList: recipeOils,
+      lyeAmount,
+    }));
+    console.log(oils, lyeAmount);
+  };
+
+  return (
+    <div className={styles.recipeFormWrapper}>
+      <div className="recipe-form-container">
+        <Form onSubmit={handleSubmitTotalOil}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control
+              type="number"
+              placeholder="Enter total weight of oil in ounces"
+              name="totalOil"
+              value={formInput.totalOil}
+              onChange={handleChange}
+            />
+            <Form.Text>Enter total weight of oils in ounces</Form.Text>
+            <Form.Control
+              type="number"
+              placeholder="Water as a percent of oils"
+              name="waterAmount"
+              value={formInput.waterAmount}
+              onChange={handleChange}
+            />
+            <Form.Text>Water as a percent of oils</Form.Text>
+            <Form.Control
+              type="number"
+              placeholder="Super Fat Percentage"
+              name="superFat"
+              value={formInput.superFat}
+              onChange={handleChange}
+            />
+            <Form.Text>Super Fat Percentage</Form.Text>
+          </Form.Group>
+        </Form>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Select name="currentOil" id="currentOil">
+              {allOils.map((oil) => <option key={oil.id} value={oil.id}>{oil.name}</option>)}
+            </Form.Select>
+            <Form.Text>Select an Oil to add</Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="button" onClick={addOils}>
+            Add Oil
+          </Button>
+        </Form>
+      </div>
+      <div className="oil-list-container">
+        <h3>Current Recipe:</h3>
+        <section>
+          <p>Total Weight:{` ${formInput.totalOil} oz.`}</p>
+        </section>
+        <section>
+          {oils?.length > 0
+            ? (
+              <>
+                <h3>Oils:</h3>
+                <Form onSubmit={calculateRecipe}>
+                  {oils?.map((oil) => (
+                    <div key={oil.id}>
+                      <h3 key={oil.id}>{oil.name}</h3>
+                      <Form.Control
+                        id={`oilAmount--${oil.id}`}
+                        name={oil.id}
+                        type="number"
+                        placeholder="Amount in ounces"
+                        onChange={handleAmountChange}
+                      />
+                    </div>
+                  ))}
+                  <Button variant="primary" type="submit">Calculate Recipe</Button>
+                </Form>
+              </>
+            )
+            : ''}
+
+        </section>
+      </div>
+    </div>
+  );
+}
--- pages/recipe/new.js
+++ pages/recipe/new.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import NewRecipeForm from '../../components/NewRecipeForm/NewRecipeForm';
+// import NewRecipeForm from '../../components/NewRecipeForm/NewRecipeForm';
+import TestRecipeForm from '../../components/NewRecipeForm/TestRecipeForm';
 
 export default function CreateRecipePage() {
   return (
     <>
       <h1>Create New Recipe</h1>
-      <NewRecipeForm />
+      <TestRecipeForm />
     </>
   );
 }
