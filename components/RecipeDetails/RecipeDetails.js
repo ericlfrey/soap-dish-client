86dd79d6ce3dcaa3e50e494faa5625622586ed00
Eric Frey <ericlfrey@gmail.com>
2023-06-11 15:13:05 -0500

    WIP. Basic info for recipe on page. Oils in the oild list are mapping correctly. Only User can see the Edit and Delete links on details card.

--- components/RecipeDetails/RecipeDetails.js
+++ components/RecipeDetails/RecipeDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import Link from 'next/link';
 import { getSingleRecipe } from '../../utils/data/recipeData';
 import { useAuth } from '../../utils/context/authContext';
 
@@ -12,20 +13,33 @@ export default function RecipeDetails({ id }) {
     getSingleRecipe(id, user.uid).then(setRecipe);
   }, [id, user]);
 
+  const handleDelete = () => {
+    console.log('RECIPE DELETED');
+  };
+
   return (
     <>
       {console.log(recipe)}
       <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>{recipe.title}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">Oils: { }</Card.Subtitle>
+          {recipe.recipe_oils?.map((oil) => (
+            <Card.Subtitle key={oil.id} className="mb-2 text-muted">{oil.oil_name}: {oil.amount} oz</Card.Subtitle>
+          ))}
           <Card.Subtitle className="mb-2 text-muted">Water: {recipe.water_amount} oz</Card.Subtitle>
           <Card.Subtitle className="mb-2 text-muted">Lye: {recipe.lye_amount} oz</Card.Subtitle>
           <Card.Subtitle className="mb-2 text-muted">Super fat: {recipe.super_fat * 100}%</Card.Subtitle>
           <Card.Text>Description: {recipe.description}</Card.Text>
           <Card.Text>Notes: {recipe.notes}</Card.Text>
-          <Card.Link href="#">Edit</Card.Link>
-          <Card.Link href="#">Delete</Card.Link>
+          {user.uid === recipe.maker?.uid
+            ? (
+              <>
+                <Link href={`/recipe/edit/${id}`} passHref>
+                  <Card.Link href="#">Edit</Card.Link>
+                </Link><Card.Link href="#" onClick={handleDelete}>Delete</Card.Link>
+              </>
+            )
+            : ''}
         </Card.Body>
       </Card>
     </>
