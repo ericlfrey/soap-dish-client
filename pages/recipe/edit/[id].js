27856597e2ff496ac56d8f0e4187ffa4fb6a69bb
Eric Frey <ericlfrey@gmail.com>
2023-06-11 18:24:51 -0500

    WIP. The form auto fills with the correct recipe info, but Im trying to get the Calculate function to work.

--- pages/recipe/edit/[id].js
+++ pages/recipe/edit/[id].js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import UpdateRecipeForm from '../../../components/NewRecipeForm/UpdateRecipeForm';
+import { getSingleRecipe } from '../../../utils/data/recipeData';
+import { useAuth } from '../../../utils/context/authContext';
 
 export default function EditRecipePage() {
+  const [recipe, setRecipe] = useState({});
+  const { user } = useAuth();
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    getSingleRecipe(id, user.uid).then(setRecipe);
+  }, [id, user.uid]);
+
+  const totalOil = parseInt(recipe.recipe_oils?.reduce((a, b) => a + b.amount, 0), 10);
+  const oilList = recipe.recipe_oils?.map((oil) => ({
+    id: oil.id,
+    name: oil.oil_name,
+    amount: oil.amount,
+  }));
+
   return (
     <>
-      <h1>EditRecipePage</h1>
+      {/* {console.log(recipe)} */}
+      <UpdateRecipeForm recipeObject={recipe} totalOil={totalOil} oilList={oilList} />
     </>
   );
 }
