b5929fd053f76a7adc504d6a8ec8c7b7a44b0928
Eric Frey <ericlfrey@gmail.com>
2023-06-11 15:01:20 -0500

    WIP. Working on Card Details component.

--- components/RecipeDetails/RecipeDetails.js
+++ components/RecipeDetails/RecipeDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { Card } from 'react-bootstrap';
 import { getSingleRecipe } from '../../utils/data/recipeData';
 import { useAuth } from '../../utils/context/authContext';
 
@@ -13,8 +14,20 @@ export default function RecipeDetails({ id }) {
 
   return (
     <>
-      <h1>Recipe Details Page</h1>
-      <h3>{recipe.title}</h3>
+      {console.log(recipe)}
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>{recipe.title}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">Oils: { }</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">Water: {recipe.water_amount} oz</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">Lye: {recipe.lye_amount} oz</Card.Subtitle>
+          <Card.Subtitle className="mb-2 text-muted">Super fat: {recipe.super_fat * 100}%</Card.Subtitle>
+          <Card.Text>Description: {recipe.description}</Card.Text>
+          <Card.Text>Notes: {recipe.notes}</Card.Text>
+          <Card.Link href="#">Edit</Card.Link>
+          <Card.Link href="#">Delete</Card.Link>
+        </Card.Body>
+      </Card>
     </>
   );
 }
