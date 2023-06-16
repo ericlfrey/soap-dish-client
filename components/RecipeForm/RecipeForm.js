53dbabe07afeeb28399558a29705f1eb466de960
Eric Frey <ericlfrey@gmail.com>
2023-06-16 11:48:16 -0500

    WIP. Still working on the form. Next step is to style calculated Recipe.

--- components/RecipeForm/RecipeForm.js
+++ components/RecipeForm/RecipeForm.js
@@ -219,9 +219,6 @@ export default function RecipeForm({ recipeObject, totalOil, oilList }) {
               </InputGroup>
               <Form.Text>Select an Oil to add</Form.Text>
             </Form.Group>
-            {/* <Button type="button" onClick={addOils}>
-              Add Oil
-            </Button> */}
           </Form>
         </div>
         <div className="oil-list-container">
@@ -236,8 +233,8 @@ export default function RecipeForm({ recipeObject, totalOil, oilList }) {
                     {oils?.map((oil) => (
                       <section key={oil.oilId}>
                         <div className={styles.oilNameBtn}>
-                          <h4 key={oil.oilId}>{`${oils.indexOf(oil) + 1}. ${oil.name}`}</h4>
-                          <Button onClick={removeOil} value={oil.oilId}>-</Button>
+                          <h4 key={oil.oilId} className={styles.oilName}>{`${oils.indexOf(oil) + 1}. ${oil.name}`}</h4>
+                          <Button onClick={removeOil} value={oil.oilId} className={styles.removeOilBtn}>remove</Button>
                         </div>
                         <Form.Group className={styles.singleOil}>
                           <Form.Control
@@ -249,7 +246,6 @@ export default function RecipeForm({ recipeObject, totalOil, oilList }) {
                             {...oil.amount > 0 ? { value: Number(oil.amount) } : { value: '' }}
                           />
                           <Form.Text>Amount in ounces</Form.Text>
-                          {/* <Button onClick={removeOil} value={oil.oilId}>-</Button> */}
                         </Form.Group>
                       </section>
                     ))}
