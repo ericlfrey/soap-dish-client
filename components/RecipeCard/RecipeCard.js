94870427a85e73cde933ab449d42d1bc01eacea6
Eric Frey <ericlfrey@gmail.com>
2023-06-17 17:49:52 -0500

    Finished styling for now. Need to clean up code before pushing.

--- components/RecipeCard/RecipeCard.js
+++ components/RecipeCard/RecipeCard.js
@@ -16,28 +16,17 @@ export default function RecipeCard({
   };
 
   return (
-    <Card className={styles.card}>
-      <Link href={`/recipe/${id}`} passHref>
-        <Card.Header className={styles.cardHeader}>
+    <Link href={`/recipe/${id}`} passHref>
+      <Card className={styles.card}>
+        {/* <Card.Header className={styles.cardHeader}>
+        </Card.Header> */}
+        <Card.Body className={styles.cardBody}>
           <Card.Title className={styles.cardTitle}>{title}</Card.Title>
-        </Card.Header>
-      </Link>
-      <Card.Body className={styles.cardBody}>
-        <Card.Subtitle className="mb-2 text-muted">{description}</Card.Subtitle>
-      </Card.Body>
-      <Card.Footer className={styles.cardFooter}>
-        {user.uid === uid
-          ? (
-            <div className={styles.cardLinks}>
-              <Link href={`/recipe/edit/${id}`} passHref>
-                <Card.Link href="#">Edit</Card.Link>
-              </Link>
-              <Card.Link href="#" onClick={handleDelete}>Delete</Card.Link>
-            </div>
-          )
-          : ''}
-      </Card.Footer>
-    </Card>
+          <Card.Text className={styles.soapIcon}>🧼</Card.Text>
+          <Card.Text className={styles.cardDetails}>{description}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 }
 
